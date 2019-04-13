33090fe611a33133372d416c10a9cf78af6593d7
subhyde <ethan_johnson@hotmail.ca>
2019-04-12 20:59:51 -0400

    completed project

---
+++ products.js
@@ -0,0 +1,123 @@
+/*eslint no-unused-vars:0*/
+var products = [
+    {
+        name: "Berry Leash",
+        img: 'src="images/leashes/berry.jpg" alt="berry leash"',
+        price: 17.99,
+        tags: "Leash",
+        tag2: "accessory",
+        popularity: 6,
+        description:
+            "A beautiful berry styled leash, perfect for your hipster dog"
+    },
+    {
+        name: "Black Leash",
+        img: 'src="images/leashes/black.jpg" alt="black leash"',
+        price: 17.99,
+        tags: "Leash",
+        tag2: "accessory",
+        popularity: 12,
+        description:
+            "Plain and simple bold black leash, make a statement... a fashion statement"
+    },
+    {
+        name: "Floral Leash",
+        img: 'src="images/leashes/floral.jpg" alt="floral leash"',
+        price: 17.99,
+        tags: "Leash",
+        tag2: "accessory",
+        popularity: 4,
+        description:
+            "A floral print leash that never looked better, show off your pups true colours"
+    },
+    {
+        name: "Olive Leash",
+        img: 'src="images/leashes/olive.jpg" alt="olive leash"',
+        price: 17.99,
+        tags: "Leash",
+        tag2: "accessory",
+        popularity: 3,
+        description:
+            "Fashion and tastefulness at its finest, minimalist and gorgeous "
+    },
+    {
+        name: "Plaid Leash",
+        img: 'src="images/leashes/plaid.jpg" alt="plaid leash"',
+        price: 17.99,
+        tags: "Leash",
+        tag2: "accessory",
+        popularity: 10,
+        description:
+            "Plaid leash for the country style dogs, sleek and gorgeous"
+    },
+    {
+        name: "Flamingo Bowtie",
+        img: 'src="images/bowties/flamingo.jpg" alt="flamingo bowtie"',
+        price: 14.99,
+        tags: "Bowties",
+        tag2: "clothing",
+        popularity: 9,
+        description:
+            "Perfect for fancy occasions, your pup will be the talk of the night"
+    },
+    {
+        name: "Floral Bowtie",
+        img: 'src="images/bowties/floral.jpg" alt="floral bowtie"',
+        price: 14.99,
+        tags: "Bowties",
+        tag2: "clothing",
+        popularity: 8,
+        description: "Floral printed bow tie, simple yet makes a statement"
+    },
+    {
+        name: "Plaid Bowtie",
+        img: 'src="images/bowties/plaidbow.jpg" alt="Plaid bowtie"',
+        price: 14.99,
+        tags: "Bowties",
+        tag2: "clothing",
+        popularity: 7,
+        description:
+            "Plaid never goes out of style. Treat your pup to a fashion trend that never dies"
+    },
+    {
+        name: "Polka Dot Collar Shirt",
+        img:
+            'src="images/collarshirts/polkadotcollar.jpg" alt="polkadot collar shirt"',
+        price: 29.99,
+        tags: "collar shirt",
+        tag2: "clothing",
+        popularity: 2,
+        description:
+            "Polka dots never looked better on a dog, statistics don't lie"
+    },
+    {
+        name: "Stripped Collar Shirt",
+        img:
+            'src="images/collarshirts/stripedcollar.jpg" alt="stripped collar shirt"',
+        price: 29.99,
+        tags: "collar shirt",
+        tag2: "clothing",
+        popularity: 1,
+        description: "Stripes on stripes on stripes. Who doesn't love stripes"
+    },
+    {
+        name: "Plaid Shirt",
+        img: 'src="images/shirts/plaidshirt.jpg" alt="plaid shirt"',
+        price: 37.99,
+        tags: "shirt",
+        tag2: "clothing",
+        popularity: 11,
+        description:
+            "The holy grail for a hunting dog, or just for a cozy lumberjack look"
+    },
+    {
+        name: "Polka Dot Shirt",
+        img: 'src="images/shirts/polkashirt.jpg" alt="Polka dot shirt"',
+        price: 37.99,
+        tags: "shirt",
+        tag2: "clothing",
+        popularity: 5,
+        description:
+            "Your pup will get all the attention in our polka-dot shirt"
+    }
+];
